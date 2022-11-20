f4aa81a7a3deb1891d0407913acc8eb78e710912
Yinan Li <gabriel.li5566@gmail.com>
2022-11-20 17:17:51 +0200

    update TestLoader

---
+++ case/testCase02.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestDemo2(unittest.TestCase):
+    def test_method1(self):
+        print("test method 2-1")
+
+    def test_method2(self):
+        print("test method 2-2")
---
+++ case/testCase03.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestDemo3(unittest.TestCase):
+    def test_method1(self):
+        print("test method 3-1")
+
+    def test_method2(self):
+        print("test method 3-2")
